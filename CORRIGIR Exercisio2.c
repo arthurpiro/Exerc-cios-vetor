c9a29035bbf0a9e478e1769f27971805f229a061
Arthur Vargas Pinheiro <269752999+arthurpiro@users.noreply.github.com>
2026-09-17 11:53:49 -0300

    Update CORRIGIR Exercisio2.c

--- CORRIGIR Exercisio2.c
+++ CORRIGIR Exercisio2.c
@@ -1,13 +1,33 @@
-// Online C compiler to run C program online
-#include <stdio.h>
+// // Online C compiler to run C program online
+// #include <stdio.h>
 
+// int main() {
+
+//     int numero[5];
+
+//     for(numero = 0; numero <= 5; numero+1) {
+//         scanf("%d", &numero[5]);
+//     for(numero = 0; numero => 5; numero-1) {
+
+//     }
+
+//     return 0;
+// }
+
+
+//possivel versão
+ // Online C compiler to run C program online
 int main() {
 
-    int numero[5];
+    int main[5];
+    int contador;
 
-    for(numero = 0; numero <= 5; numero+1) {
-        scanf("%d", &numero[5]);
-    for(numero = 0; numero => 5; numero-1) {
+    for(contador = 0; contador < 5; contador+1) {
+        scanf("%d", & contador[main]);
+        printf("%d", contador[main]);
+    // for(numero = 0; numero > 5; numero-1) {
+    //     scanf("%d", & numero[5]);
+            
 
     }
 
