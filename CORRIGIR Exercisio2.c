3afe2fa025518b4ee74ba3309e0f078677e3ecbe
Arthur Vargas Pinheiro <269752999+arthurpiro@users.noreply.github.com>
2026-09-17 11:45:28 -0300

    Create CORRIGIR Exercisio2.c

---
+++ CORRIGIR Exercisio2.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+
+    int numero[5];
+
+    for(numero = 0; numero <= 5; numero+1) {
+        scanf("%d", &numero[5]);
+    for(numero = 0; numero => 5; numero-1) {
+
+    }
+
+    return 0;
+}
